a110d0dfde0fe978963a2eb2b54561a0ed36f7f2
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-05-27 16:42:54 -0300

    clonando objetos

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 let listaDeItens = [];
+
+const form = document.getElementById('form-itens');
+const itenInput = document.getElementById('receber-item');
+
+form.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    salvarItem();
+})
+
+function salvarItem(){
+    const compraItem = itenInput.value;
+
+    listaDeItens.push( {
+        valor: compraItem,
+    })
+    console.log(listaDeItens);
+}
