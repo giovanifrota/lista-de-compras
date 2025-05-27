e51881d1fab4b6c39d9ae150bf5c785e2bb326e5
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-05-27 16:55:49 -0300

    Comparando objetos

--- main.js
+++ main.js
@@ -10,9 +10,14 @@ form.addEventListener('submit', function(evento){
 
 function salvarItem(){
     const compraItem = itenInput.value;
+    const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === compraItem.toUpperCase());
 
+    if(checarDuplicado){
+        alert("Item já existe!")
+    }else{
     listaDeItens.push( {
         valor: compraItem,
     })
+}
     console.log(listaDeItens);
 }
