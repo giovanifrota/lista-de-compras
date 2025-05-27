50e4db98d3f4a193bc43d429f6424ec164716727
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-05-27 17:29:14 -0300

    Definindo métodos

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 let listaDeItens = [];
 
-const form = document.getElementById('form-itens');
-const itenInput = document.getElementById('receber-item');
+const form = document.getElementById("form-itens");
+const itenInput = document.getElementById("receber-item");
+const ulItems = document.getElementById("lista-de-itens");
 
-form.addEventListener('submit', function(evento){
-    evento.preventDefault();
-    salvarItem();
-})
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  salvarItem();
+  mostrarItem();
+});
 
-function salvarItem(){
-    const compraItem = itenInput.value;
-    const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === compraItem.toUpperCase());
+function salvarItem() {
+  const compraItem = itenInput.value;
+  const checarDuplicado = listaDeItens.some(
+    (elemento) => elemento.valor.toUpperCase() === compraItem.toUpperCase()
+  );
 
-    if(checarDuplicado){
-        alert("Item já existe!")
-    }else{
-    listaDeItens.push( {
-        valor: compraItem,
-    })
+  if (checarDuplicado) {
+    alert("Item já existe!");
+  } else {
+    listaDeItens.push({
+      valor: compraItem,
+    });
+  }
+  console.log(listaDeItens);
 }
-    console.log(listaDeItens);
+
+function mostrarItem() {
+    ulItems.innerHTML = "";
+  listaDeItens.forEach((elemento, index) => {
+    ulItems.innerHTML += `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>`;
+  });
 }
